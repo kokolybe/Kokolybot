9572a0a4631987f8494fb89bdb0be6fec4b63812
kokolybe <52601492+kokolybe@users.noreply.github.com>
2024-11-24 11:36:38 +0300

    Update core.py

--- core.py
+++ core.py
@@ -7,19 +7,22 @@ chat = Messenger()
 
 @ampalibe.command('/')
 def main(sender_id, cmd, **ext):
+    # Envoyer un message texte
     chat.send_text(sender_id, "Hello, Ampalibe")
+    
+    # Créer des quick replies
     quick_rep = [
-    QuickReply(
-        title="Angela",
-        payload=Payload("/membre"),
-        image_url="https://i.imgflip.com/6b45bi.jpg"
-    ),
-    QuickReply(
-        title="Rivo",
-        payload=Payload("/membre"),
-        image_url="https://i.imgflip.com/6b45bi.jpg"
-    ),
-]
+        QuickReply(
+            title="Angela",
+            payload=Payload("/membre"),
+            image_url="https://i.imgflip.com/6b45bi.jpg"
+        ),
+        QuickReply(
+            title="kouly",
+            payload=Payload("/membre"),
+            image_url="https://i.imgflip.com/6b45bi.jpg"
+        ),
+    ]
 
-# next=True in parameter for displaying directly next list quick_reply
-chat.send_quick_reply(sender_id, quick_rep, 'who do you choose ?') 
+    # Envoyer les quick replies
+    chat.send_quick_reply(sender_id, quick_rep, 'Who do you choose?')
