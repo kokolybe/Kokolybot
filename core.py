527e77af6de57da194bde3bbee65f31261f4c24b
kokolybe <52601492+kokolybe@users.noreply.github.com>
2024-11-25 20:55:34 +0300

    Update core.py

--- core.py
+++ core.py
@@ -17,7 +17,7 @@ def setup(sender_id, **ext):
     persistent_menu = [
         Button(type=Type.postback, title='Menu', payload=Payload('/menu')),
         Button(type=Type.postback, title='Musique', payload=Payload('/spotify')),
-        Button(type=Type.postback, title='Spotify🎶', payload=Payload('/spotify_search))
+        Button(type=Type.postback, title='Spotify🎶', payload=Payload('/spotify_search'))
     ]
     chat.persistent_menu(sender_id, persistent_menu)
     chat.send_text(sender_id, "Le menu persistant a été configuré avec succès !")
